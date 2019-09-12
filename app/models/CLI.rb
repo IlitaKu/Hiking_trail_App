65206c05b66e51dc1f1355e74a0e9a76d0ee07b8
github username <github email address>
2019-09-12 11:40:03 +0100

    almostFinal

--- app/models/CLI.rb
+++ app/models/CLI.rb
@@ -42,12 +42,12 @@ class CLI
         # binding.pry
     end
     def commands
-        command = @@prompt.select("Please select the following:", ["See hiking trail locations", "See your reviews", "Update your reviews", "Delete your account", "Exit"])
+        command = @@prompt.select("Please select the following:", ["See hiking trail locations", "Read Hiking Trail reviews", "See your reviews", "Update your reviews", "Delete your account", "Exit"])
         if command == "See hiking trail locations"
             hiking_trail_locations
-        # elsif 
-        #     command == "Write a review"
-        #     write_review
+        elsif 
+            command == "Read Hiking Trail reviews"
+            see_reviews
         elsif 
             command == "See your reviews"
             see_reviews
@@ -77,6 +77,7 @@ class CLI
         trail = Hiking_Trail.find_by(location: selected_place)
         content = @@prompt.ask("Tell us what you think about #{selected_place} trail:", required: true)
         rating = @@prompt.ask("please rate your experience on a scale of 1 to 5:", required: true)
+        @@prompt.say("Thank you for participation!")
         # Review.create(trail_id: trail.id, user_id: @@user.id, rating: rating, content: content)
         Review.create([{content: content, rating: rating, hiking_trail_id: trail.id, user_id: @@user.id}])
         # binding.pry
@@ -84,9 +85,17 @@ class CLI
     end
 
     def see_reviews
-        @personal_reviews = Review.select{|review|review.user_id == @@user.id}
-        @review_names = @personal_reviews.map{|review| review.content}
-        @user_review = @@prompt.select("Here is all the reviews we got from you so far!", @review_names)
+        all_trails = Hiking_Trail.all.map{|place| place.location}
+        chosen_trail= @@prompt.select("Select the hiking trail to see reviews", all_trails)
+        trail_object = Hiking_Trail.all.find_by(location: chosen_trail)
+        selected_reviews = Review.all.select{|t| t.hiking_trail_id == trail_object.id}.map{|t| t.content}
+        
+        i = 0
+        while selected_reviews.length > i do
+            puts "Review #{i + 1}. #{selected_reviews[i]}"
+            i += 1
+        end
+        commands
     end
 
     def update_review
@@ -94,16 +103,27 @@ class CLI
         @review_names = @personal_reviews.map{|review| review.content}
         @content = @@prompt.select("Here is all the reviews we got from you so far!", @review_names)
         @selected_review = Review.find_by(content: @content)
-        user_choice = @@prompt.select("What would you like to update?", ["content", "rating"])
+        user_choice = @@prompt.select("What would you like to update?", ["content", "rating"]
+                
         if user_choice == "content"
             new_content = @@prompt.ask("What is the new content?",required:true)
             @selected_review.update(content: new_content)
+        elsif 
+            user_choice == "rating"
+            new_rating = @@prompt.ask("What is the new content?",required:true)
+            @selected_review.update(rating: new_rating)
         end
+        @@prompt.say("Your review has been updated")
+        commands
     end
+
     def delete_account
-        delete_account = @@prompt.ask("If you want to delete your account type 'DESTROY'")
-        if delete_account == 'DESTROY'
+        delete_account = @@prompt.yes?("Are you sure that you want to delete your account?", require: true)
+        if delete_account 
         @@user.delete
+        @@prompt.say("Your accout has been deleted.")
+        elsif
+            @@prompt.say("Phew..that was close! Thank you for staying with us #{@@user.name.capitalize}!")
         end
     end
 end
